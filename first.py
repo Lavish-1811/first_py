85f7aba8a28b292cf198e99950c63db195a1dfb2
Lavish <lavishgupta1811@gmail.com>
2026-06-26 11:49:49 +0530

    f_1 third commit

--- first.py
+++ first.py
@@ -6,3 +6,6 @@ print("Hello this is f_1 world and ruling by him ")
 
 print("Hello this is f_1 world and ruling by him ")
 # this is change by f_1 later in the second commit 
+
+print("Hello sir this is the correct way to do it ")
+# CORRECTING THE MISTAKE MADE IN THE PREVIOUS COMMIT
