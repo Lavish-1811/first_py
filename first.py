033cd8e66c219c3416297ee87fa05ddf782671ad
Lavish <lavishgupta1811@gmail.com>
2026-06-26 11:46:46 +0530

    Second commit

--- first.py
+++ first.py
@@ -3,3 +3,6 @@ print("Hello world")
 
 print("Hello this is f_1 world and ruling by him ")
 # this is change by f_1 branch
+
+print("Hello this is f_1 world and ruling by him ")
+# this is change by f_1 later in the second commit 
